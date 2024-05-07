b9fca0ab1dc5a1a3365faf721300d34cb5aa72e4
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-05-07 19:34:05 +0700

    change devise default routes' pathname

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :vendors
-  devise_for :users
+  devise_for :vendors, path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
+  devise_for :users, path: "/", path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
