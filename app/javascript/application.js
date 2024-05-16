f2ab5fe24406d265f760da47c794cb84bd3d2e21
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-05-16 23:35:18 +0700

    better way of handling Turbo:missing-frame error without set reload the login page everytime user visit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,22 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "trix"
-import "@rails/actiontext"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "trix";
+import "@rails/actiontext";
 
-import * as ActiveStorage from "@rails/activestorage"
+import * as ActiveStorage from "@rails/activestorage";
 
-ActiveStorage.start()
+ActiveStorage.start();
+
+const delayInMilliseconds = 200;
+Turbo.setProgressBarDelay(delayInMilliseconds);
+
+document.addEventListener("turbo:frame-missing", (event) => {
+  event.preventDefault();
+  // console.log("Frame missing event", event);
+
+  // if the url of the response is /login, then it means the user is not
+  // logged in when request was made, so we redirect to the login page
+  if (event.detail.response.url.includes("/login"))
+    event.detail.visit("/login", { action: "replace" });
+});
