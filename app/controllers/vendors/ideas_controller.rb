eceb4b7bbf7773d969db28dcfb1c00a2c79097a2
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-05-25 17:38:24 +0700

    Avoiding N+1 Queries

--- app/controllers/vendors/ideas_controller.rb
+++ app/controllers/vendors/ideas_controller.rb
@@ -1,28 +1,20 @@
 class Vendors::IdeasController < Vendors::BaseController
-  before_action :set_idea, only: %i[ edit update destroy ]
+  before_action :set_idea, only: %i[ update destroy publish unpublish ]
+  before_action :set_idea_with_rich_text_content_and_embeds, only: %i[ edit show ]
 
-  # GET /ideas or /ideas.json
   def index
-    @ideas = Idea.all
+    @ideas = current_vendor.ideas.with_rich_text_content_and_embeds
   end
 
-  # GET /ideas/1 or /ideas/1.json
-  def show
-    # Avoiding N+1 Queries
-    @idea = Idea.with_rich_text_content_and_embeds.find(params[:id])
-  end
+  def show; end
 
-  # GET /ideas/new
   def new
     @idea = Idea.new
   end
 
-  # GET /ideas/1/edit
   def edit; end
 
-  # POST /ideas or /ideas.json
   def create
-    # @idea = Idea.new(idea_params)
     @idea = current_vendor.ideas.build(idea_params)
 
     respond_to do |format|
@@ -36,7 +28,6 @@ class Vendors::IdeasController < Vendors::BaseController
     end
   end
 
-  # PATCH/PUT /ideas/1 or /ideas/1.json
   def update
     respond_to do |format|
       if @idea.update(idea_params)
@@ -49,7 +40,6 @@ class Vendors::IdeasController < Vendors::BaseController
     end
   end
 
-  # DELETE /ideas/1 or /ideas/1.json
   def destroy
     @idea.destroy!
 
@@ -60,8 +50,6 @@ class Vendors::IdeasController < Vendors::BaseController
   end
 
   def publish
-    @idea = current_vendor.ideas.find(params[:id])
-
     # if the idea is not paid, it can't be published
     unless @idea.is_paid?
       flash.now[:error] = "You need to pay for this idea before publishing it."
@@ -69,7 +57,7 @@ class Vendors::IdeasController < Vendors::BaseController
       return
     end
 
-    if @idea.update(published: true)
+    if @idea.update_attribute(:published, true)
       flash.now[:notice] = "Idea was successfully published."
     else
       flash.now[:error] = "Something went wrong. Please try again."
@@ -79,9 +67,7 @@ class Vendors::IdeasController < Vendors::BaseController
   end
 
   def unpublish
-    @idea = current_vendor.ideas.find(params[:id])
-
-    if @idea.update(published: false)
+    if @idea.update_attribute(:published, false)
       flash.now[:notice] = "Idea was successfully unpublished."
     else
       flash.now[:error] = "Something went wrong. Please try again."
@@ -92,12 +78,15 @@ class Vendors::IdeasController < Vendors::BaseController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_idea
-    @idea = Idea.find(params[:id])
+    @idea = current_vendor.ideas.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
+  def set_idea_with_rich_text_content_and_embeds
+    # Avoiding N+1 Queries
+    @idea = current_vendor.ideas.with_rich_text_content_and_embeds.find(params[:id])
+  end
+
   def idea_params
     params.require(:idea).permit(:title, :description, :main_image, :content, :topic_id)
   end
