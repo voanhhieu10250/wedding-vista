aca98be8c4dcc28c729e98b5305aa6cbf190927d
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-05-31 00:24:29 +0700

    update

--- app/models/priority_boosting.rb
+++ app/models/priority_boosting.rb
@@ -6,7 +6,7 @@ class PriorityBoosting < ApplicationRecord
   enum status: { pending: "PENDING", active: "ACTIVE", expired: "EXPIRED" }
 
   def update_times_and_job(start_time, end_time)
-    SolidQueue::Job.find_by(active_job_id: job_id).destroy!
+    SolidQueue::Job.find_by(active_job_id: job_id)&.destroy! if job_id.present?
     job_id = ActivateBoostingJob.set(wait_until: start_time).perform_later(id)
 
     update(start_time:, end_time:, job_id:)
