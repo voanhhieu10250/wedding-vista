969404fea610fc760502e38150967de6e17f5dc7
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-06-11 19:20:03 +0700

    done UI for homepage (without the responsiveness)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,7 @@ class PagesController < ApplicationController
     @ideas = Idea.includes(:vendor, :topic).with_attached_main_image
                  .where(published: true)
                  .order(created_at: :desc).limit(4)
+
+    @discussions = Discussion.with_rich_text_body.order(id: :desc).limit(4)
   end
 end
