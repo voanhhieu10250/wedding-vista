e9961d45e4472c79693ea8c1171c932f60343462
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-06-16 17:53:00 +0700

    use cache_store for storing session data instead of cookie_store

---
+++ config/initializers/sessions.rb
@@ -0,0 +1,7 @@
+# Using redis to manage sessions (same cache appliance as standard rails caching)
+# IMPORTANT: "expire_after" should match "timeout_in" config in: config/initializers/devise.rb
+unless Rails.application.config.cache_store == :null_store
+  Rails.application.config.session_store ActionDispatch::Session::CacheStore,
+                                         key: "_wedding_vista_session",
+                                         expire_after: 2.weeks
+end
