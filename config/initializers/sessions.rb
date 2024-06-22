242707493102890665d83ee59cb9c1ccc0ae4ff0
Vo Anh Hieu <voanhhieu10250@gmail.com>
2024-06-23 03:59:59 +0700

    fix broken font-family

--- config/initializers/sessions.rb
+++ config/initializers/sessions.rb
@@ -1,4 +1,5 @@
-# Using redis to manage sessions (same cache appliance as standard rails caching)
+# Using the same db store for caching to manage sessions (same cache appliance as standard rails caching)
+# If you're using redis for caching, you can use that redis store for sessions as well.
 # IMPORTANT: "expire_after" should match "timeout_in" config in: config/initializers/devise.rb
 unless Rails.application.config.cache_store == :null_store
   Rails.application.config.session_store ActionDispatch::Session::CacheStore,
